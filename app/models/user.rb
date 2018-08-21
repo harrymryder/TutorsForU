f4dee631e0bf34ff148989ad8d669149e6bd6884
Chitty Liu <d2921118@gmail.com>
2018-08-21 18:36:31 +0100

    creat meeting is done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
 
   has_many :user_subjects
   has_many :subjects, through: :user_subjects
-  has_many :meetings
+  has_many :meetings_where_is_student, class_name: 'Meeting', primary_key: 'id', foreign_key: 'student_id'
+  has_many :meetings_where_is_tutor, class_name: 'Meeting', primary_key: 'id', foreign_key: 'tutor_id'
+
 
   def tutor?
     self.subjects.any?
