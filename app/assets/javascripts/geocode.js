840dacaba79e9f48bda1ec0c327b2d89702cb34a
Harry Ryder <harrymryder@gmail.com>
2018-08-22 16:48:53 +0100

    precompiles assets

--- app/assets/javascripts/geocode.js
+++ app/assets/javascripts/geocode.js
@@ -1,16 +1,16 @@
 
-document.addEventListener("keyup", (event) => {
- if (event.key === "Enter") {
-   const searchQuery = document.getElementById("search");
-   const query = searchQuery.value;
-   const list = document.getElementById("results");
-   fetch(`https://nominatim.openstreetmap.org/search/${query}?format=json&limit=1`)
-   .then(response => response.json())
-   .then((data) => {
-      let lat = Number.parseFloat(data[0].lat)
-      let long = Number.parseFloat(data[0].lon)
-      list.insertAdjacentHTML("beforeend", `<li>Latitude: ${lat}</li>`);
-      list.insertAdjacentHTML("beforeend", `<li>Longitude: ${long}</li>`);
-    })
- }
-})
+// document.addEventListener("keyup", (event) => {
+//  if (event.key === "Enter") {
+//    const searchQuery = document.getElementById("search");
+//    const query = searchQuery.value;
+//    const list = document.getElementById("results");
+//    fetch(`https://nominatim.openstreetmap.org/search/${query}?format=json&limit=1`)
+//    .then(response => response.json())
+//    .then((data) => {
+//       let lat = Number.parseFloat(data[0].lat)
+//       let long = Number.parseFloat(data[0].lon)
+//       list.insertAdjacentHTML("beforeend", `<li>Latitude: ${lat}</li>`);
+//       list.insertAdjacentHTML("beforeend", `<li>Longitude: ${long}</li>`);
+//     })
+//  }
+// })
