65c8a13c2aa3b2aede70e073eee12c7d6196948f
Chitty Liu <d2921118@gmail.com>
2018-08-22 11:29:18 +0100

    geocoding and description, image migration done in User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_104925) do
+ActiveRecord::Schema.define(version: 2018_08_22_102806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 2018_08_21_104925) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "location"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "description"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
